ac53d62fb3536ac07a7f988b677715702ce703b3
Chaz Leong <13462818+cleong14@users.noreply.github.com>
2024-04-28 00:44:55 -1000

    feat: set ollama num_ctx
    Signed-off-by: Chaz Leong <13462818+cleong14@users.noreply.github.com>

--- lua/codecompanion/adapters/ollama.lua
+++ lua/codecompanion/adapters/ollama.lua
@@ -151,8 +151,19 @@ First think step-by-step - describe your plan for what to build in pseudocode, w
         return n >= 0 and n <= 2, "Must be between 0 and 2"
       end,
     },
+    num_ctx = {
+      order = 3,
+      mapping = "parameters.options",
+      type = "number",
+      optional = true,
+      default = 16384,
+      desc = "Sets the size of the context window used to generate the next token. (Default: 2048)",
+      validate = function(n)
+        return n >= 2048, "Must be greater than or equal to 2048"
+      end,
+    },
     -- top_p = {
-    --   order = 3,
+    --   order = 4,
     --   mapping = "parameters.options",
     --   type = "number",
     --   optional = true,
@@ -160,17 +171,6 @@ First think step-by-step - describe your plan for what to build in pseudocode, w
     --   desc = "An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered. We generally recommend altering this or temperature but not both.",
     --   validate = function(n)
     --     return n >= 0 and n <= 1, "Must be between 0 and 1"
-    --   end,
-    -- },
-    -- num_ctx = {
-    --   order = 4,
-    --   mapping = "parameters.options",
-    --   type = "number",
-    --   optional = true,
-    --   default = 16384,
-    --   desc = "Sets the size of the context window used to generate the next token. (Default: 2048)",
-    --   validate = function(n)
-    --     return n >= 2048, "Must be greater than or equal to 2048"
     --   end,
     -- },
   },
